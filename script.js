718747230f6bc472cb3a95b77f59f537a184302f
dhearr <dhearr27@gmail.com>
2024-01-24 11:50:04 +0700

    first update

---
+++ script.js
@@ -0,0 +1,27 @@
+// =====================================================================================================
+// // DOM MANIPULATION
+// const card = document.querySelector(".card");
+// const close = document.querySelector(".close");
+// close.addEventListener("click", function () {
+//   card.style.visibility = "hidden";
+// });
+// =====================================================================================================
+
+// =====================================================================================================
+// // DOM TRAVERSAL
+// // Menggunakan FOR
+// const close = document.querySelectorAll(".close");
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener("click", function (event) {
+//     event.target.parentElement.style.display = "none";
+//   });
+// }
+
+// // Menggunakan FOREACH
+// const close = document.querySelectorAll(".close");
+// close.forEach(function (element) {
+//   element.addEventListener("click", function (event) {
+//     event.target.parentElement.style.display = "none";
+//   });
+// });
+// =====================================================================================================
